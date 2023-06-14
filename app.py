9bed7df6baf5ce3ec04c2538c991aaef9b4bc6db
alocke8181 <alocke_8181@yahoo.com>
2023-06-14 15:33:28 -0400

    cleanup and css

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-from flask import Flask, request, render_template, redirect, flash, session
+from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
 from forms import New_Pet, Edit_Pet
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField, BooleanField
-from wtforms.validators import InputRequired, Optional
 
 
 app = Flask(__name__)
@@ -16,16 +13,17 @@ app.config['SECRET_KEY'] = 'secret'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 
-
 connect_db(app)
 
 @app.route('/')
 def home():
-    pets = Pet.query.all()
+    """Show the homepage sorted by availability and alphabetically"""
+    pets = Pet.query.order_by(Pet.available.desc(), Pet.name).all()
     return render_template('home.html', pets=pets)
 
 @app.route('/add', methods=['GET','POST'])
 def add_pet():
+    """Show the Add Pet Form and handle the post request"""
     form = New_Pet()
     if form.validate_on_submit():
         name = form.name.data
@@ -43,7 +41,8 @@ def add_pet():
         return render_template('add_pet.html', form=form)
 
 @app.route('/<int:pet_id>', methods=['GET','POST'])
-def pet_details(pet_id):
+def show_pet_details(pet_id):
+    """Show details of each pet as well as the edit form"""
     pet = Pet.query.get(pet_id)
     form = Edit_Pet(obj=pet)
     if form.validate_on_submit():
@@ -52,6 +51,7 @@ def pet_details(pet_id):
         pet.available = form.available.data
         db.session.add(pet)
         db.session.commit()
+        flash('Pet updated','notif')
         return redirect(f'/{pet.id}') 
     else:
         return render_template('pet_details.html', pet=pet, form=form)
--- forms.py
+++ forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, IntegerField
+from wtforms import StringField, BooleanField, IntegerField, TextAreaField
 from wtforms.validators import InputRequired, Optional, NumberRange, URL, AnyOf
 
 
 class New_Pet(FlaskForm):
-
+    """Form for creating new pets"""
     name = StringField('Name', validators=[InputRequired()])
 
     species = StringField('Species', validators=[InputRequired(), AnyOf(['Cat','Dog','Porcupine'])])
@@ -13,14 +13,14 @@ class New_Pet(FlaskForm):
 
     age = IntegerField('Age', validators=[Optional(), NumberRange(min=0, max=30, message="Age must be between 0 and 30")])
 
-    notes = StringField('Notes')
+    notes = TextAreaField('Notes', render_kw = {'rows':5, 'cols':50})
 
     available = BooleanField('Available')
     
 class Edit_Pet(FlaskForm):
-
+    """Form for editing existing pets"""
     photo_url = StringField('Photo URL', validators=[Optional(), URL()])
 
-    notes = StringField('Notes')
+    notes = TextAreaField('Notes', render_kw = {'rows':5, 'cols':50})
 
     available = BooleanField('Available')
